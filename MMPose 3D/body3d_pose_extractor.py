13154b24aaa5505bd22b708ce37e8e48d42874f6
jmartinezot@gmail.com <jmartinezot@gmail.com>
2024-03-21 18:40:01 +0100

    Saving mmpose results as pandas dataframe

--- MMPose 3D/body3d_pose_extractor.py
+++ MMPose 3D/body3d_pose_extractor.py
@@ -4,17 +4,23 @@ python body3d_pose_extractor.py  \
 --input mmpose_3d_examples/two_people_standing.jpg \
 --output-root  /tmp \
 --save-predictions
+python body3d_pose_extractor.py  \
+--input /tmp/Scenario2-2.avi \
+--output-root  /tmp/scenario \
+--save-predictions
+python body3d_pose_extractor.py  \
+--input mmpose_3d_examples/basketball.mp4 \
+--output-root  /tmp \
+--save-predictions
 '''
 '''
 import pickle as pkl
-pred_save_pickle_path = "vis_results/results_two_people_standing.pkl"
-pred_save_pickle_path = "vis_results/results_164970135-b14e424c-765a-4180-9bc8-fa8d6abc5510.pkl"
-pred_save_pickle_path = "results_two_people_standing.pkl"
-with open(pred_save_pickle_path, 'rb') as f:
-    a = pkl.load(f)
-with open(pred_save_pickle_path, 'rb') as f:
-    pred_instances_list, bboxes_2d_list = pkl.load(f)
-print(pred_instances_list[0])
+pred_save_pickle_path_image = "results_two_people_standing.pkl"
+pred_save_pickle_path_video = "results_basketball.pkl"
+with open(pred_save_pickle_path_image, 'rb') as f:
+    image_data = pkl.load(f)
+with open(pred_save_pickle_path_video, 'rb') as f:
+    video_data = pkl.load(f)
 '''
 from processimage import process_one_image
 import logging
@@ -24,6 +30,7 @@ import time
 from argparse import ArgumentParser
 from functools import partial
 import pickle as pkl
+import pandas as pd
 
 import cv2
 import json_tricks as json
@@ -187,11 +194,20 @@ def main():
     elif input_type in ['webcam', 'video']:
         next_id = 0
         pose_est_results = []
+        frames_to_save = []
+        frame_count = 0
 
         if args.input == 'webcam':
             video = cv2.VideoCapture(0)
         else:
             video = cv2.VideoCapture(args.input)
+            frame_count = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
+            print_log(
+                f'Video {args.input} has {frame_count} frames in total.',
+                logger='current',
+                level=logging.INFO)
+            pred_instances_list = [[] for _ in range(frame_count)]
+            frames_to_save = [[] for _ in range(frame_count)]
 
         (major_ver, minor_ver, subminor_ver) = (cv2.__version__).split('.')
         if int(major_ver) < 3:
@@ -205,7 +221,9 @@ def main():
         while video.isOpened():
             success, frame = video.read()
             frame_idx += 1
-
+            print_log(f'Processing frame {frame_idx} / {frame_count}',
+                      logger='current',
+                      level=logging.INFO) 
             if not success:
                 break
 
@@ -215,7 +233,6 @@ def main():
             # make person results for current image
             (pose_est_results, pose_est_results_list, pred_3d_instances,
              next_id, bboxes_2d) = process_one_image(
-                 args=args,
                  detector=detector,
                  frame=frame,
                  frame_idx=frame_idx,
@@ -225,13 +242,14 @@ def main():
                  next_id=next_id,
                  pose_lifter=pose_lifter,
                  visualize_frame=mmcv.bgr2rgb(frame),
-                 visualizer=visualizer)
+                 visualizer=visualizer,
+                 show=show)
 
             if args.save_predictions:
                 # save prediction results
-                pred_instances_list.append(
-                    dict(
-                        frame_id=frame_idx,
+                if input_type == 'video':
+                    pred_instances_list[frame_idx - 1] = dict(
+                        frame_id=frame_idx - 1,
                         instances_3d=split_instances(pred_3d_instances),
                         instances_2d=[{
                             'keypoints': data_sample.pred_instances.keypoints,
@@ -240,25 +258,44 @@ def main():
                         } for data_sample in pose_est_results],  # Add 2D keypoints and scores here
                         bboxes_2d=bboxes_2d
                     )
-                )
+                else: # webcam
+                    pred_instances_list.append(
+                        dict(
+                            frame_id=frame_idx,
+                            instances_3d=split_instances(pred_3d_instances),
+                            instances_2d=[{
+                                'keypoints': data_sample.pred_instances.keypoints,
+                                'keypoint_scores': data_sample.pred_instances.keypoint_scores,
+                                'track_id': data_sample.get('track_id', None)
+                            } for data_sample in pose_est_results],  # Add 2D keypoints and scores here
+                            bboxes_2d=bboxes_2d
+                        )
+                    )
             if save_output:
                 frame_vis = visualizer.get_image()
-                if video_writer is None:
-                    # the size of the image with visualization may vary
-                    # depending on the presence of heatmaps
-                    video_writer = cv2.VideoWriter(output_file, fourcc, fps,
-                                                   (frame_vis.shape[1],
-                                                    frame_vis.shape[0]))
+                if input_type == 'webcam':
+                    if video_writer is None:
+                        # the size of the image with visualization may vary
+                        # depending on the presence of heatmaps
+                        video_writer = cv2.VideoWriter(output_file, fourcc, fps,
+                                                    (frame_vis.shape[1],
+                                                        frame_vis.shape[0]))
+                    video_writer.write(mmcv.rgb2bgr(frame_vis))
+                else: # video
+                    frames_to_save[frame_idx - 1] = frame_vis
 
-                video_writer.write(mmcv.rgb2bgr(frame_vis))
-
-            if args.show:
+            if show:
                 # press ESC to exit
                 if cv2.waitKey(5) & 0xFF == 27:
                     break
                 time.sleep(args.show_interval)
 
         video.release()
+        if save_output and input_type == 'video':
+            video_writer = cv2.VideoWriter(output_file, fourcc, fps, 
+                                   (frames_to_save[0].shape[1], frames_to_save[0].shape[0]))
+            for idx, frame_vis in enumerate(frames_to_save):
+                video_writer.write(mmcv.rgb2bgr(frame_vis))
 
         if video_writer:
             video_writer.release()
@@ -276,8 +313,30 @@ def main():
                 f,
                 indent='\t')
         print(f'predictions have been saved at {pred_save_path}')
+
+        # This list will hold one dictionary for each skeleton across all frames
+        skeletons_data = []
+
+        for data in pred_instances_list:
+            frame_id = data['frame_id']
+            bboxes_2d = data['bboxes_2d']
+
+            for instance_3d, instance_2d, bbox_2d in zip(data['instances_3d'], data['instances_2d'], bboxes_2d):
+                skeleton_data = {
+                    'frame_id': frame_id,
+                    '3d_keypoints': instance_3d['keypoints'],
+                    '3d_keypoints_scores': instance_3d['keypoint_scores'],
+                    '2d_keypoints': instance_2d['keypoints'][0].tolist(),  # Convert numpy array to list for DataFrame compatibility
+                    '2d_keypoints_scores': instance_2d['keypoint_scores'][0].tolist(),  # Convert numpy array to list
+                    'track_id': instance_2d['track_id'],
+                    'bboxes_2d': bbox_2d[0].tolist()  # Convert numpy array to list
+                }
+                skeletons_data.append(skeleton_data)
+
+        # Create a DataFrame from the list of dictionaries
+        df = pd.DataFrame(skeletons_data)
         with open(pred_save_pickle_path, 'wb') as f:
-            pkl.dump((pred_instances_list),f)
+            pkl.dump((pred_instances_list, df),f)
 
     if save_output:
         input_type = input_type.replace('webcam', 'video')
